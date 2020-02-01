a1caec2bff954a71beb4459ddd68703c858834e0
Laredo Tirnanic <LaredoT@webuycars.co.za>
2020-02-01 13:49:20 +0200

    Update docker port

--- Docker/docker_run.sh
+++ Docker/docker_run.sh
@@ -3,7 +3,7 @@ docker rm single-sign-on
 docker run \
 	--name=single-sign-on \
 	-d --restart unless-stopped \
-	-p 5001:80 \
+	-p 5000:80 \
 	-e "ConnectionStrings__DefaultConnection"="Host=localhost;Database=SingleSignOn;Username=postgres;Password=password1;" \
 	-e "DatabaseType"="Postgres" \
 	laredoza/single-sign-on:latest \
